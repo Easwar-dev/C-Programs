84c8f23cb1285a6841aa7f7317dbd03019b99a1b
Easwar <host57203@gmail.com>
2025-11-01 11:54:22 +0530

    finish with lot of bugs

--- Bus_res_system.c
+++ Bus_res_system.c
@@ -135,4 +135,73 @@ void checkBusStatus(struct Bus buses[], int numBuses){
     }
 }
 
-//main function 
+//main function
+int main(){
+    // Initialize user data
+    struct User users[5] = {
+                {"user1", "pass1"}, {"user2", "pass2"}, {"user3, pass3"}, {"user4","pass4"}, {"user5", "pass5"}
+    };
+            int numUsers = 5;
+
+            //initialize bus data
+            struct Bus buses[3] = {
+                {101, "City A", "City B", 50, 50, 500.0},
+                {101, "City C", "City D", 40, 40, 400.0},
+                {101, "City E", "City F", 30, 30, 300.0},
+            };
+            int numBuses = 3;
+
+            int loggedInUserId = -1; //index of the logged-in user
+
+            while(1){
+                if(loggedInUserId == -1){
+                    displayMainMenu();
+                    int choice;
+                    scanf("%d", &choice);
+
+                    if(choice == 1){
+                        char username[50];
+                        char password[50];
+
+                        printf("Enter Username: ");
+                        scanf("%s", username);
+                        printf("Enter Password: ");
+                        scanf("%s", password);
+
+                        loggedInUserId = loginUser(users, numUsers, username, password);
+                        if(loggedInUserId == -1){
+                            printf("Login failed. Please check your username and password.\n");
+                        }else{
+                            printf("Login successful. Welcome, %s!\n", username);
+                        }
+                    }else if(choice == 2){
+                        printf("Exiting the program.\n");
+                        break;
+                    }else{
+                        printf("Invalid choice. please try again.\n");
+                    }
+                }else{
+                    displayMainMenu();
+                    int userChoice;
+                    scanf("%d", &userChoice);
+
+                    switch(userChoice){
+                        case 1:
+                            bookTicket(buses, numBuses);
+                            break;
+                        case 2:
+                            cancelTicket(buses, numBuses);
+                            break;
+                        case 3:
+                            checkBusStatus(buses, numBuses);
+                            break;
+                        case 4:
+                            printf("Logging out.\n");
+                            break;
+                        default:
+                            printf("Invalid choice. Please try again.\n");
+                    }
+                }
+            }
+    return 0;
+}
